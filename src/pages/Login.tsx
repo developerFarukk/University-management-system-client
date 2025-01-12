b349e6aaa6e7b542c93b8e034628f2194b242a66
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-12 20:30:58 +0600

    navigat success

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,8 +6,12 @@ import { useLoginMutation } from '../redux/features/auth/authApi';
 import { useAppDispatch } from '../redux/hooks';
 import { setUser } from '../redux/features/auth/authSlice';
 import { verifyToken } from '../utils/verifyToken';
+import { useNavigate } from 'react-router-dom';
+
 
 const Login = () => {
+    const navigate = useNavigate();
+
     const dispatch = useAppDispatch();
     const { register, handleSubmit } = useForm({
         defaultValues: {
@@ -16,7 +20,7 @@ const Login = () => {
         },
     });
 
-    const [login ] = useLoginMutation();
+    const [login] = useLoginMutation();
 
     const onSubmit = async (data: any) => {
         const userInfo = {
@@ -25,26 +29,31 @@ const Login = () => {
         };
 
         console.log(userInfo);
-        
+
 
         const res = await login(userInfo).unwrap();
         const user = verifyToken(res.data.accessToken);
 
         dispatch(setUser({ user: user, token: res.data.accessToken }));
+        navigate(`/${user.role}/dashboard`);
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <label htmlFor="id">ID: </label>
-                <input type="text" id="id" {...register('userId')} />
-            </div>
-            <div>
-                <label htmlFor="password">Password: </label>
-                <input type="text" id="password" {...register('password')} />
-            </div>
-            <Button htmlType="submit">Login</Button>
-        </form>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
+                    <label htmlFor="id">ID: </label>
+                    <input type="text" id="id" {...register('userId')} />
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="text" id="password" {...register('password')} />
+                </div>
+                <Button htmlType="submit">Login</Button>
+
+            </form>
+
+        </div>
     );
 };
 
