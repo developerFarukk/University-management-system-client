4b26e103cb3b632eae27aa945d853d68e83753dd
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-18 01:30:46 +0600

    Add User type

--- src/redux/features/admin/userManagement.api.ts
+++ src/redux/features/admin/userManagement.api.ts
@@ -1,3 +1,4 @@
+import { TQueryParam, TResponseRedux } from "../../../types";
 import { baseApi } from "../../api/baseApi";
 
 
@@ -5,30 +6,30 @@ const userManagementApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
 
         // Get All Student API
-        // getAllStudents: builder.query({
-        //     query: (args) => {
-        //         console.log(args);
-        //         const params = new URLSearchParams();
+        getAllStudents: builder.query({
+            query: (args) => {
+                console.log(args);
+                const params = new URLSearchParams();
 
-        //         if (args) {
-        //             args.forEach((item: TQueryParam) => {
-        //                 params.append(item.name, item.value as string);
-        //             });
-        //         }
+                if (args) {
+                    args.forEach((item: TQueryParam) => {
+                        params.append(item.name, item.value as string);
+                    });
+                }
 
-        //         return {
-        //             url: '/students',
-        //             method: 'GET',
-        //             params: params,
-        //         };
-        //     },
-        //     transformResponse: (response: TResponseRedux<TStudent[]>) => {
-        //         return {
-        //             data: response.data,
-        //             meta: response.meta,
-        //         };
-        //     },
-        // }),
+                return {
+                    url: '/students',
+                    method: 'GET',
+                    params: params,
+                };
+            },
+            transformResponse: (response: TResponseRedux<TStudent[]>) => {
+                return {
+                    data: response.data,
+                    meta: response.meta,
+                };
+            },
+        }),
 
         // create student API
         addStudent: builder.mutation({
