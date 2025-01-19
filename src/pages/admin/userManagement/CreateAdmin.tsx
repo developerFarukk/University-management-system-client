7eb120c08dfc27773cc39d6511f4e68c7879bd25
FARUK2059 <omarfaruk34552@gmail.com>
2025-01-20 04:34:03 +0600

    add data

--- src/pages/admin/userManagement/CreateAdmin.tsx
+++ src/pages/admin/userManagement/CreateAdmin.tsx
@@ -1,6 +1,11 @@
+// import { 
+//     useGetAllAdminsQuery, 
+//     useGetAllStudentsQuery
+//  } from "../../../redux/features/admin/userManagement.api";
 
 
 const CreateAdmin = () => {
+
     return (
         <div>
             <p>This is Create admin</p>
